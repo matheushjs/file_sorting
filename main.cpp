3a53a04b1eea9a26f65d309dbcf8db72171c37d1
Matheus Saldanha <matheus.saldanha@usp.br>
2017-07-09 22:36:14 -0300

    Add benchmark in seconds

--- main.cpp
+++ main.cpp
@@ -8,10 +8,14 @@
 using std::clock_t;
 using std::clock;
 using std::cout;
+using std::ofstream;
+using std::chrono::system_clock;
+using std::chrono::time_point;
+using std::chrono::duration;
 
 int main(int argc, char *argv[]){
 	MyDatabase db;
-	std::ofstream file;
+	ofstream file;
 
 	/*
 	std::ifstream infile;
@@ -24,25 +28,32 @@ int main(int argc, char *argv[]){
 	 */
 
 	clock_t beg, end;
+	time_point<system_clock> begt, endt;
 
 	// db.printAll();
 
 	file.open("output.db", file.in | file.trunc);
 
+	begt = system_clock::now();
 	beg = clock();
 	db.normal_heapsort(file);
-	file.flush()
+	file.flush();
 	end = clock();
+	endt = system_clock::now();
 	cout << "Normal heapsort (clocks): " << (end-beg) / (double) CLOCKS_PER_SEC << '\n';
+	cout << "Normal heapsort (time): " << duration<double>(endt-begt).count() << '\n';
 
 	file.close();
 	file.open("output.db", file.in | file.trunc);
 
+	begt = system_clock::now();
 	beg = clock();
 	db.parallel_heapsort(file);
 	file.flush();
 	end = clock();
+	endt = system_clock::now();
 	cout << "Parallel heapsort (clocks): " << (end-beg) / (double) CLOCKS_PER_SEC << '\n';
+	cout << "Parallel heapsort (time): " << duration<double>(endt-begt).count() << '\n';
 
 	return 0;
 }
